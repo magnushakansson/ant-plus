12dc2098f7bebf856eb0116591c4b451a11e2f13
Magnus Håkansson <gho@qlik.com>
2018-03-11 14:48:49 +0100

    Fix problem in speed-and-cadence sample

--- sample/speed-and-cadence-sensor.js
+++ sample/speed-and-cadence-sensor.js
@@ -7,20 +7,27 @@ const speedSensor = new Ant.SpeedSensor(stick);
 speedSensor.setWheelCircumference(2.120); //Wheel circumference in meters
 
 speedSensor.on('speedData', data => {
-  console.log(`speed: ${data.CalculatedSpeed}`);
+    console.log(`speed: ${data.CalculatedSpeed}`);
 });
 
 cadenceSensor.on('cadenceData', data => {
-  console.log(`cadence: ${data.CalculatedCadence}`);
+    console.log(`cadence: ${data.CalculatedCadence}`);
 });
 
+speedSensor.on('attached', function() { console.log('SpeedSensor attached'); });
+speedSensor.on('detached', function() { console.log('SpeedSensor detached'); });
+
+cadenceSensor.on('attached', function() { console.log('CadenceSensor attached'); });
+cadenceSensor.on('detached', function() { console.log('CadenceSensor detached'); });
 
 stick.on('startup', function () {
-	console.log('startup');
-	speedSensor.attach(0, 0);
-	cadenceSensor.attach(0, 0);
+   console.log('startup');
+   speedSensor.attach(0, 0);
+   setTimeout(function(data) {
+       cadenceSensor.attach(1, 0);
+   }, 2000);
 });
 
 if (!stick.open()) {
-	console.log('Stick not found!');
+        console.log('Stick not found!');
 }
